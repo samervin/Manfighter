1c5e47d9c576fac0da7dd3a3d78364331e4919b7
samervin <samuelthomaservin@gmail.com>
2014-01-28 21:49:39 -0600

    Greatly expanded enemy names

--- src/person/Enemy.java
+++ src/person/Enemy.java
@@ -4,8 +4,11 @@ import game.Person;
 import game.RandGen;
 import game.Weapon;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public abstract class Enemy extends Person {
 
@@ -72,25 +75,72 @@ public abstract class Enemy extends Person {
 
 	protected String createRandomName() {
 		String n = "";
-		int x = RandGen.getRand(1, 3);
-		switch(x) {
-		case 1: n += "Dr. "; break;
-		}
-
-		x = RandGen.getRand(1,3);
-		switch(x) {
-		case 1: n += "Willem"; break;
-		case 2: n += "Skeletor"; break;
-		case 3: n += "Neo"; break;
-		default: n += "ERROR";
-		}
-
-		x = RandGen.getRand(1,8);
-		switch(x) {
-		case 1: n+= " DaFoe"; break;
-		case 2: n+= " the Outrageous"; break;
-		case 3: n+= " the Particularly Smelly"; break;
-		case 4: n+= " the Terrible"; break;
+		int x = RandGen.getRand(1, 50);
+		if(x == 50) {
+			Scanner special;
+			try {
+				special = new Scanner(new File("src/data/name_special.txt"));
+				int max = Integer.parseInt(special.nextLine()) -1; //off-by-one errors
+				int maxRand = RandGen.getRand(0, max);
+				for(int i = 0; i < maxRand; i++) {
+					special.nextLine();
+				}
+				n += special.nextLine();
+				special.close();
+				
+			} catch (FileNotFoundException e) {
+				n = "name_special.txt not found";
+			}
+			
+		} else {
+			if(x % 4 == 0) {
+				Scanner prefix;
+				try {
+					prefix = new Scanner(new File("src/data/name_prefix.txt"));
+					int max = Integer.parseInt(prefix.nextLine()) -1; //off-by-one errors
+					int maxRand = RandGen.getRand(0, max);
+					for(int i = 0; i < maxRand; i++) {
+						prefix.nextLine();
+					}
+					n += prefix.nextLine() + " ";
+					prefix.close();
+					
+				} catch (FileNotFoundException e) {
+					n = "name_prefix.txt not found";
+				}
+			}
+			
+			Scanner first;
+			try {
+				first = new Scanner(new File("src/data/name_first.txt"));
+				int max = Integer.parseInt(first.nextLine()) -1; //off-by-one errors
+				int maxRand = RandGen.getRand(0, max);
+				for(int i = 0; i < maxRand; i++) {
+					first.nextLine();
+				}
+				n += first.nextLine();
+				first.close();
+				
+			} catch (FileNotFoundException e) {
+				n = "name_first.txt not found";
+			}
+			
+			if(x < 45) {
+				Scanner last;
+				try {
+					last = new Scanner(new File("src/data/name_last.txt"));
+					int max = Integer.parseInt(last.nextLine()) -1; //off-by-one errors
+					int maxRand = RandGen.getRand(0, max);
+					for(int i = 0; i < maxRand; i++) {
+						last.nextLine();
+					}
+					n += " " + last.nextLine();
+					last.close();
+					
+				} catch (FileNotFoundException e) {
+					n = "name_last.txt not found";
+				}
+			}
 		}
 
 		return n;
